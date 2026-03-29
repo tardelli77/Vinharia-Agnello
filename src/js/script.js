cc0ff1b978d5141c7014ac5cbcce322a75b138a3
Gabriel Tardelli <tardelligabriel55@gmail.com>
2026-03-29 20:02:10 -0300

    feat: exibindo o cadastro do usuário

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,8 @@ let cadastro = prompt('Digite seu nome para fazer um cadastro: ');
 let senha = prompt('Digite sua senha para fazer um cadastro: ');
  
 alert('Cadastro realizado! Veja os detalhes no console.');
+
+console.log('-----------CADASTRO REALIZADO!-----------');
+console.log(`Nome de usuário: ${cadastro}`);
+console.log(`Senha: ${senha}`);
+console.log('-------------FIM DO CADASTRO-------------');
